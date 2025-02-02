45d4f193ca236a8fdf750d4e070388a8111e4083
Vasek2024 <p.f1@yandex.ru>
2025-02-02 21:19:24 +0700

    Слайдер с точками, стрелками и ссылками

---
+++ js/slaider.js
@@ -0,0 +1,65 @@
+const slidesDots = document.getElementsByClassName('result__img'),
+    btnNext = document.querySelector('.description__revers-back'),
+    btnPrev = document.querySelector('.description__revers-forward'),
+    wrappetDots = document.querySelector('.description__revers-pagination'),
+    dots = document.getElementsByClassName('description__revers-btn'),
+    wrappelincs = document.querySelector('.result__nav'),
+    navLincs = document.getElementsByClassName('result__list')
+let slideIndex = 1
+
+// Вызов функции
+showSlides(slideIndex)
+
+function showSlides(index) {
+    if (index > slidesDots.length) {// если index больне длинны массива слайдов, то
+        slideIndex = 1
+    }
+    if (index < 1) {// если index меньше одного, то
+        slideIndex = slidesDots.length// равно длинне массива
+    }
+    for (let i = 0; i < slidesDots.length; i++) {// проходим массивом по слайдам
+        slidesDots[i].style.display = 'none'// задаём стиль (прячем все слайды)
+    }
+    for (let i = 0; i < dots.length; i++) {// проходим массивом по точкам
+        dots[i].classList.remove('active')// удаляем класс активности у кнопак
+    }
+    for (let i = 0; i < navLincs.length; i++) {// проходим массивом по ссылкам
+        navLincs[i].classList.remove('active')// удаляем класс активности у ссылок
+    }
+    slidesDots[slideIndex - 1].style.display = 'block'// задаём стиль слайду (показываем)
+    dots[slideIndex - 1].classList.add('active')// задаём класс активности у кнопаки
+    navLincs[slideIndex - 1].classList.add('active')// задаём класс активности у ссылоки
+}
+
+function pluseSlaide(index) {// прибавить слайд
+    showSlides(slideIndex += index)
+}
+
+function currentSlaide(index) {// индекс текущего слайда
+    showSlides(slideIndex = index);
+}
+
+btnPrev.addEventListener('click', function () {// событие клика по стрелки назад
+    pluseSlaide(-1)// отнимаем слайд
+})
+btnNext.addEventListener('click', function () {// событие клика по стрелки вперёд
+    pluseSlaide(1)// прибавляем слайд
+})
+
+wrappetDots.addEventListener('click', function (event) {// событие клика по блоку с точками
+    for (let i = 0; i < dots.length + 1; i++) {// проходим по нему массивом
+        // если кликнули именно по точке и 
+        if (event.target.classList.contains('description__revers-btn') && event.target == dots[i - 1]) {
+            currentSlaide(i)// индекс текущего слайда
+        }
+    }
+})
+
+wrappelincs.addEventListener('click', function (event) {// событие клика по блоку со ссылками
+    for (let i = 0; i < navLincs.length + 1; i++) {// проходим по нему массивом
+        // если кликнули именно по ссылке и 
+        if (event.target.classList.contains('result__list') && event.target == navLincs[i - 1]) {
+            currentSlaide(i)// индекс текущего слайда
+        }
+    }
+})
